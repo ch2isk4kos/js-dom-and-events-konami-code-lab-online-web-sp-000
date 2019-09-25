e47c296fb45b49f09f5b2eac131ae6eb2d51e5b9
ch2isk4kos <ch2isk4kos@gmail.com>
2019-09-25 18:39:51 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+    let counter = 0;
+
+    document.body.addEventListener('keydown', function(element) {
+
+        const propertyKey = parseInt(element.detail);
+
+        if (code[counter] === propertyKey) {
+            counter++;
+
+            if (counter === code.length) {
+                alert('Hurray!');
+
+                counter = 0;
+            }
+        } else {
+            counter = 0;
+        }
+
+    });
 }
+
+init()
